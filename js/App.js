c89156de1db5d2176e6a7ca2d5b375eb1bcbfc5c
Vincent Wang <a36168120@gmail.com>
2019-08-20 09:24:35 -0700

    init commit

---
+++ js/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>This is R10</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
