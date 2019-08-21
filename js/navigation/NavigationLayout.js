f6138abf03d6501afb2dfeffae18b71b15871c89
Vincent Wang <a36168120@gmail.com>
2019-08-20 17:35:17 -0700

    Setup navigations for screens

---
+++ js/navigation/NavigationLayout.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import AboutScreen from '../screens/About';
+import FavesScreen from '../screens/Faves';
+import MapScreen from '../screens/Map';
+import ScheduleScreen from '../screens/Schedule';
+import SessionScreen from '../screens/Session';
+// import SpeakerScreen from '../screens/Speaker';
+import {sharedNavigationOptions} from './config';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const AboutStack = createStackNavigator(
+  {
+    About: AboutScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+    }),
+  },
+);
+
+const MapStack = createStackNavigator({
+  About: {
+    screen: MapScreen,
+  },
+});
+
+const FavesStack = createStackNavigator({
+  About: {
+    screen: FavesScreen,
+  },
+
+  Session: {
+    screen: SessionScreen,
+  },
+
+  // Speaker: {
+  //   screen: SpeakerScreen,
+  // },
+});
+
+const ScheduleStack = createStackNavigator({
+  Schedule: {
+    screen: ScheduleScreen,
+  },
+
+  Session: {
+    screen: SessionScreen,
+  },
+
+  // Speaker: {
+  //   screen: SpeakerScreen,
+  // },
+});
+
+export default createBottomTabNavigator(
+  {
+    Schedule: ScheduleStack,
+    Map: MapStack,
+    Faves: FavesStack,
+    About: AboutStack,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        const {routeName} = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Schedule') {
+          iconName = `ios-calendar`;
+        } else if (routeName === 'Map') {
+          iconName = `ios-map`;
+        } else if (routeName === 'Faves') {
+          iconName = `ios-heart`;
+        } else if (routeName === 'About') {
+          iconName = `ios-information-circle-outline`;
+        }
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: '#FFFFFF',
+      inactiveTintColor: '#808080',
+
+      labelStyle: {
+        fontSize: 12,
+        // fontFamily: 'Montserrat',
+      },
+
+      style: {
+        backgroundColor: 'blue',
+      },
+    },
+  },
+);
--- js/navigation/RootStackNavigator.js
+++ js/navigation/RootStackNavigator.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
-import AboutScreen from '../screens/About';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+import NavigationLayout from './NavigationLayout';
+import SpeakerModal from '../screens/Speaker';
 
-const AboutScreen = createStackNavigator({
-  Home: {
-    screen: AboutScreen,
-  },
-});
-
-export default AboutScreen;
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Layout: NavigationLayout,
+      Speaker: SpeakerModal,
+    },
+    {
+      headerMode: 'none',
+      mode: 'modal',
+    },
+  ),
+);
