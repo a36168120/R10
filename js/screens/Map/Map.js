12c9277537f0969917daf773f38a349931a4b798
Vincent Wang <a36168120@gmail.com>
2019-08-20 17:31:10 -0700

    re-formate Map screen

---
+++ js/screens/Map/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const Map = () => {
+  return (
+    <View>
+      <Text>Map</Text>
+    </View>
+  );
+};
+
+export default Map;
---
+++ js/screens/Map/MapContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class MapContainer extends Component {
+  render() {
+    return (
+      <View>
+        <Text> MapContainer </Text>
+      </View>
+    );
+  }
+}
+
+export default MapContainer;
