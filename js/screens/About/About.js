9c06754b8dc32c87eb2bb77fdcd7ccafb7a7d35e
Vincent Wang <a36168120@gmail.com>
2019-08-22 15:51:42 -0700

    update style for Home screen

--- js/screens/About/About.js
+++ js/screens/About/About.js
@@ -10,29 +10,40 @@ const About = ({conduct}) => {
           style={styles.image}
           source={require('../../assets/images/r10_logo.png')}
         />
-
-        {Platform.OS === 'ios' ? (
-          // <Text style={styles.test}>
-          //   R10 is a conference that focuses on just about any topic related to
-          //   dev.
-          // </Text>
+        <View style={styles.line} />
+        {/* {Platform.OS === 'ios' ? (
           <Text style={styles.ios}>This iPhone</Text>
         ) : (
           <Text style={styles.android}>This is Android</Text>
-        )}
-        <Text style={styles.h2}>Date &amp; Venue</Text>
+        )} */}
+
+        <Text style={styles.text}>
+          R10 is a conference that focuses on just about any topic related to
+          dev.
+        </Text>
+
+        <Text style={styles.header}>Date &amp; Venue</Text>
+
         <Text style={styles.text}>
           The R10 conference will take place on Thursday, June 27, 2017 in
           Vancouver, BC.
         </Text>
-        <Text style={styles.h2}>Code of Conduct</Text>
+
+        <Text style={styles.header}>Code of Conduct</Text>
 
         {conduct.allConducts.map(conduct => (
           <View key={conduct.id}>
-            <Text>{conduct.title}</Text>
-            <Text>{conduct.description}</Text>
+            <Text style={styles.title}>+ {conduct.title}</Text>
+            {/* <Text>{conduct.description}</Text> */}
           </View>
         ))}
+
+        {Platform.OS === 'ios' && (
+          <View>
+            <View style={styles.line} />
+            <Text style={styles.text}>ⓒ RED Academy 2019</Text>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- js/screens/About/styles.js
+++ js/screens/About/styles.js
@@ -1,34 +1,64 @@
 import {StyleSheet, Plateform} from 'react-native';
 import {typography} from '../../config/styles';
 
-const {fontMain} = typography;
+const {fontMain, fontMainLight} = typography;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+
   image: {
     height: 100,
     width: '100%',
     resizeMode: 'center',
   },
 
-  ios: {
-    fontFamily: fontMain,
+  line: {
+    width: '100%',
+    borderBottomColor: '#e6e6e6',
+    borderBottomWidth: 1,
   },
 
-  android: {
-    fontFamily: fontMain,
+  header: {
+    fontSize: 30,
+    ...Platform.select({
+      android: {
+        fontFamily: fontMain,
+        color: '#999999',
+      },
+      ios: {
+        fontFamily: fontMain,
+        color: 'black',
+      },
+    }),
   },
 
-  // text: {
-  //   fontSize: 28,
-  //   ...Plateform.select({
-  //     ios: {
-  //       color: 'red',
-  //     },
-  //     android: {
-  //       color: 'blue',
-  //     },
-  //   }),
-  // },
+  text: {
+    fontSize: 16,
+    paddingTop: 20,
+    paddingBottom: 20,
+    ...Platform.select({
+      android: {
+        fontFamily: fontMain,
+        color: '#C0C0C0',
+      },
+      ios: {
+        fontFamily: fontMain,
+        color: 'black',
+      },
+    }),
+  },
+
+  title: {
+    fontSize: 16,
+    paddingTop: 20,
+    paddingBottom: 20,
+    color: '#9963ea',
+    fontFamily: 'Montserrat-Regular',
+  },
 });
 
 export default styles;
